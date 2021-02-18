11b28e6935d4e4adf0724ecdb9e11bf46d170a6c
liulihaocai <liulihaocai@outlook.com>
2021-02-18 15:02:15 +0800

    fix compatibility

--- src/main/java/me/liuli/cm/ChatListener.java
+++ src/main/java/me/liuli/cm/ChatListener.java
@@ -1,11 +1,14 @@
 package me.liuli.cm;
 
+import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerChatEvent;
 import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.event.player.PlayerQuitEvent;
+import cn.nukkit.event.server.DataPacketReceiveEvent;
+import cn.nukkit.network.protocol.TextPacket;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,35 +32,45 @@ public class ChatListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onChat(PlayerChatEvent event){
-        UUID uuid=event.getPlayer().getUniqueId();
-        String msg=event.getMessage().toLowerCase().replaceAll("\\s*", "");
-
-        for(String words:ChatManager.ignore){
-            msg=msg.replaceAll(words,"");
-        }
-
-        if((System.currentTimeMillis()-chatTime.get(uuid))<ChatManager.delay){
-            event.setCancelled();
-            event.getPlayer().sendMessage(ChatManager.dl_warn);
+    public void onPacket(DataPacketReceiveEvent event){
+        Player player=event.getPlayer();
+        if(player==null){
             return;
         }
-        chatTime.put(uuid,System.currentTimeMillis());
+        //some trash chat plugins ignore event is cancelled
+        //so I'll cancel the chat packet
+        if(event.getPacket() instanceof TextPacket){
+            TextPacket packet=(TextPacket) event.getPacket();
 
-        if(chatMsg.get(uuid).equals(msg)){
-            event.setCancelled();
-            event.getPlayer().sendMessage(ChatManager.rp_warn);
-            return;
-        }
-        chatMsg.put(uuid,msg);
+            UUID uuid=event.getPlayer().getUniqueId();
+            String msg=packet.message.toLowerCase().replaceAll("\\s*", "");
 
-        msg=Pinyin.toPinyin(msg);
-        for(String words:ChatManager.banWords) {
-            if(msg.contains(words)){
+            for(String words:ChatManager.ignore){
+                msg=msg.replaceAll(words,"");
+            }
+
+            if((System.currentTimeMillis()-chatTime.get(uuid))<ChatManager.delay){
                 event.setCancelled();
-                String prohWord=ChatManager.oriBanWords.get(ChatManager.banWords.indexOf(words));
-                event.getPlayer().sendMessage(ChatManager.bw_warn.replaceAll("%w%",prohWord));
+                event.getPlayer().sendMessage(ChatManager.dl_warn);
                 return;
+            }
+            chatTime.put(uuid,System.currentTimeMillis());
+
+            if(chatMsg.get(uuid).equals(msg)){
+                event.setCancelled();
+                event.getPlayer().sendMessage(ChatManager.rp_warn);
+                return;
+            }
+            chatMsg.put(uuid,msg);
+
+            msg=Pinyin.toPinyin(msg);
+            for(String words:ChatManager.banWords) {
+                if(msg.contains(words)){
+                    event.setCancelled();
+                    String prohWord=ChatManager.oriBanWords.get(ChatManager.banWords.indexOf(words));
+                    event.getPlayer().sendMessage(ChatManager.bw_warn.replaceAll("%w%",prohWord));
+                    return;
+                }
             }
         }
     }
